046856dee65314d38984ba253b57888510a8e380
spiders22v <spiders22v@gmail.com>
2024-01-26 15:58:32 +0900

    update

---
+++ docker/example/app.py
@@ -0,0 +1,26 @@
+print("Hello, World!")
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 0부터 2파이까지의 값을 0.1 간격으로 생성
+x = np.arange(0, 2 * np.pi, 0.1)
+
+# 사인과 코사인 값을 계산
+y_sin = np.sin(x)
+y_cos = np.cos(x)
+
+# 그래프 생성 및 데이터 플로팅
+plt.plot(x, y_sin, label='sin(x)')
+plt.plot(x, y_cos, label='cos(x)')
+
+# 그래프에 제목과 레이블 추가
+plt.title('Sine and Cosine Functions')
+plt.xlabel('x')
+plt.ylabel('y')
+
+# 범례 표시
+plt.legend()
+
+# 그래프 표시
+plt.show()
---
+++ docker/example/ggd.py
@@ -0,0 +1,5 @@
+for i in range(2, 10):
+    print(f"{i}단")
+    for j in range(1, 10):
+        print(f"{i} x {j} = {i * j}")
+    print()  # 한 단 출력 후 빈 줄 추가
